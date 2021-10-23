c42315a39fe7169b26f007df46e6dc93f2006dbf
hwal Izen <metalhwal@gmail.com>
2021-10-23 15:54:32 +0900

    첫수업 종료

---
+++ C_Workspace/C_Project_01/C_Project_01/print number as binary.c
@@ -0,0 +1,78 @@
+#define BINARY_PATTERN_INT8 "%c%c%c%c_%c%c%c%c"
+#define BYTE_TO_BINARY_INT8(i) \
+  (((i) & 0x80ll) ? '1' : '0'), \
+  (((i) & 0x40ll) ? '1' : '0'), \
+  (((i) & 0x20ll) ? '1' : '0'), \
+  (((i) & 0x10ll) ? '1' : '0'), \
+  (((i) & 0x08ll) ? '1' : '0'), \
+  (((i) & 0x04ll) ? '1' : '0'), \
+  (((i) & 0x02ll) ? '1' : '0'), \
+  (((i) & 0x01ll) ? '1' : '0')
+
+#define BINARY_PATTERN_INT16 \
+	BINARY_PATTERN_INT8  BINARY_PATTERN_INT8
+
+#define BYTE_TO_BINARY_INT16(i) \
+	BYTE_TO_BINARY_INT8((i) >> 8), BYTE_TO_BINARY_INT8(i)
+
+#define BINARY_PATTERN_INT32 \
+	BINARY_PATTERN_INT16 BINARY_PATTERN_INT16
+
+#define BYTE_TO_BINARY_INT32(i) \
+	BYTE_TO_BINARY_INT16((i) >> 8), BYTE_TO_BINARY_INT16(i)
+
+#define BINARY_PATTERN_INT64 \
+	BINARY_PATTERN_INT16 BINARY_PATTERN_INT16
+
+#define BYTE_TO_BINARY_INT64(i) \
+	BYTE_TO_BINARY_INT32((i) >> 8), BYTE_TO_BINARY_INT32(i)
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c_%c%c%c%c"
+#define BINARY_OCT_CHUNK(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
+//#define EMP {'0','0','0','0','0','0','0','0', '\0'};
+//
+//#define AAAA(num) num >> 8	
+//
+//#define BBBB(num) \
+//	((num >> 8) > 0 ? BINARY_OCT_CHUNK(num	 >> 8) : EMP), \
+//	((num ) > 0 ? BINARY_OCT_CHUNK(num) : EMP)
+
+void main() {
+
+	binprint(8);
+	binprint(780); binprint(9990);
+	binprint(76313);
+	//char aaa[] = { '0','0','0','0','0','0','0','0', '\0' };
+	//printf("%s", aaa);
+	//printf("------------- %s", BBBB(128));
+	//printf("wowowowowow  "BYTE_TO_BINARY_PATTERN"  "BYTE_TO_BINARY_PATTERN, BBBB(128));
+	//printf("%d\n", AAAA(256));
+	//printf("%d", AAAA(520));
+}
+
+
+int binprint(int num) {
+	int front, back;
+	front = num >> 8;
+	if (front > 0) {
+		back = num - (front << 8);
+		printf(" "BYTE_TO_BINARY_PATTERN, BINARY_OCT_CHUNK(front));
+		binprint(back);
+	}
+	if (front == 0) {
+		printf(" "BYTE_TO_BINARY_PATTERN"\n", BINARY_OCT_CHUNK(num));
+		return 0;
+	}
+}
--- C_Workspace/C_Project_01/C_Project_01/test_04.c
+++ C_Workspace/C_Project_01/C_Project_01/test_04.c
@@ -2,8 +2,122 @@
 #include <stdio.h>
 
 void main() {
+	//test_04();
+	//test_05();
+	//test_07();
+	//test_08();
+	//test_09();
+	//test_10();
+	test_11();
+	test_12();
+
+}
+
+int test_04() {
 	printf("My age is :  %d\n", 29);
 	printf("%d is my point", 100);// 100 is my point
 	printf("Good\tMorning\r\neverybody\n");
 	printf("Good\tMorning\r\neverybody\r\n");
+	return 0;
+}
+
+int test_05() {
+	printf("4 * 5 = %d\n", 4 * 5);
+	printf("7 * 9 = %d\n", 7 * 9);
+	multiply(12, 12);
+	return 0;
+}
+
+int multiply(int a, int b) {
+	printf("%d * %d = %d\n", a, b, a * b);
+	return 0;
+}
+
+int test_07() {
+	int height;
+	int weight;
+	int cha;
+	height = 168;
+	weight = 46;
+	cha = height - weight;
+	printf("    키 : %d cm\n", height);
+	printf("몸무게 : %d kg\n", weight);
+	printf("키에서 몸무게를 뺀 값은 %d 입니다.\n", cha);
+	return 0;
+}
+
+int test_08() {
+	int num, num2;
+	printf("정수값 하나를 입력하시어요~~:   ");
+	scanf_s("%d", &num);
+	printf("Decimal: %012d, Octal: %09o, Hexadecimal: %#X\n", num, num, num);
+
+	printf("정수값 두 개를 입력하시어요~~:   ");
+	scanf_s("%d %d", &num, &num2);
+	printf("입력받은 정수값은 %d, %d입니다.\n", num, num2);
+	return 0;
+}
+
+int test_09() {
+	int bonbong, bonus, total;
+	double tax, real_income;
+
+	bonbong = 150;
+	bonus = 60;
+
+	total = bonbong + bonus;
+	tax = total * 0.08; 
+	real_income = total - tax;
+
+	printf("본봉: %d\t보너스: %d\t총급여: %d\n", bonbong, bonus, total);
+	printf("세금: %6.3lf\t실수입액: %.2lf\n", tax, real_income);
+	printf("%-8d\n%+8d", 11, -44);
+	return 0;
+}
+
+int test_10() { // 전기요금 계산
+	double base_fee = 660;
+	double rate_kw = 88.5;
+	double tax, fee, total;
+	double usage;
+	
+	printf(" 전기 사용량을 입력하세요 (Kw) : ");
+	scanf_s("%lf", &usage);
+
+	fee = base_fee + (usage * rate_kw);
+	tax = fee * 0.09;
+	total = fee + tax;
+	
+	printf(" 전기 사용요금은 %.3lf원 입니다.\n", total);
+	return 0;
+}
+
+/* scanf 자료형 맞추기
+*  
+* double	>	lf	> 64bit	!! double complies with the IEC 60559:1989 (IEEE 754) standard for binary floating - point arithmetic. 
+* float		>	f	> 32bit 
+* 
+* 
+*/
+
+int test_11() {
+	// ASCII 코드 표
+	printf("정수형 상수값: %d\n", 65);
+	printf("상수값을 문자로: %c\n", 65);
+	printf("문자를 숫자로: %d\n", 'A'); // 리터럴 'A'를 출력시에 %d로 하면 숫자로 출력된다.
+	return 0;
+}
+
+int test_12() {
+	// assign CHAR
+	char ch = 'A';
+	const xx = "ㄴㄸㅆ";  /// const의 자료형은 어떻게 결정되는가????
+	
+	printf("ch:%c\n", ch);
+	printf("literal int size: %d byte (8 bit)\n", sizeof(7));
+	printf("literal double size: %d byte\n", sizeof(42321513547.654516517));
+	printf("literal char size: %d byte\n", sizeof('A'));
+	printf("assigned char size: %d byte\n", sizeof(ch));
+
+	return 0;
 }
